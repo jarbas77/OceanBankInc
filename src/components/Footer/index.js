f8cf13411cbb91b1404c042853bb0d584dfa17a5
jarbas77 <david_silva_94@live.com.pt>
2021-07-13 20:34:57 +0100

    OceanBankInc.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { animateScroll as scroll } from 'react-scroll';
+import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin,} from "react-icons/fa";
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinksItems, FooterLinksTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink,} from './FooterElement';
+
+const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  }
+
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinksItems>
+              <FooterLinksTitle>About us</FooterLinksTitle>
+                <FooterLink to='/'>How it works</FooterLink>
+                <FooterLink to='/'>Testimonials</FooterLink>
+                <FooterLink to='/'>Carrers</FooterLink>
+                <FooterLink to='/'>Investor</FooterLink>
+                <FooterLink to='/'>Terms of Service</FooterLink>
+            </FooterLinksItems>
+            <FooterLinksItems>
+              <FooterLinksTitle>Contact Us</FooterLinksTitle>
+                <FooterLink to='/'>Contact</FooterLink>
+                <FooterLink to='/'>Support</FooterLink>
+                <FooterLink to='/'>Lost or stolen cards</FooterLink>
+                <FooterLink to='/'>Trouble logging on to Online Banking</FooterLink>
+            </FooterLinksItems>
+          </FooterLinksWrapper>
+
+          <FooterLinksWrapper>
+            <FooterLinksItems>
+              <FooterLinksTitle>Videos</FooterLinksTitle>
+                <FooterLink to='/'>Submit Video</FooterLink>
+                <FooterLink to='/'>Ambassadors</FooterLink>
+                <FooterLink to='/'>Agency</FooterLink>
+                <FooterLink to='/'>Influencer</FooterLink>
+            </FooterLinksItems>
+            <FooterLinksItems>
+              <FooterLinksTitle>Social Media</FooterLinksTitle>
+                <FooterLink to='/'>Instagram</FooterLink>
+                <FooterLink to='/'>Facebook</FooterLink>
+                <FooterLink to='/'>Youtube</FooterLink>
+                <FooterLink to='/'>Twitter</FooterLink>
+            </FooterLinksItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/' onClick={toggleHome}>
+              Ocean Bank Inc.
+            </SocialLogo>
+            <WebsiteRights>Ocean Bank Inc. © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href='//' target='_blank' arial-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='//' target='_blank' arial-label='Instagram'>
+                <FaInstagram/>
+              </SocialIconLink>
+              <SocialIconLink href='//' target='_blank' arial-label='Youtube'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href='//' target='_blank' arial-label='Twitter'>
+                <FaTwitter/>
+              </SocialIconLink>
+              <SocialIconLink href='//' target='_blank' arial-label='Linkedin'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,48 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine:'Premium Bank',
+    headline: 'UNLIMITED Transactions with ZERO fees',
+    description: 'With Ocean Banking App, you can manage your accounts anytime,anywhere.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'SmallCar',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine:'Unlimited Access',
+    headline: 'Take control of your money ',
+    description: 'Our app allows you to make payments, move money between accounts and view balances and transactions from your phone.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-6.svg').default,
+    alt: 'piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine:'Join Our Team',
+    headline: 'Creating an account is extremely easy!',
+    description: "Get everything set up and ready in under 10 minutes. All you need to do is add your personal information and you're ready to go" ,
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'SmallCar',
+    dark: false,
+    primary: false,
+    darkText: true
+};
